515a1ae06b177ff241a9909e1c09c9c8b5cd112a
fishyshyne0060 <znxdynasti@gmail.com>
2025-05-25 06:35:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const toggleBtn = document.getElementById('toggle-btn');
 const sidebar = document.getElementById('sidebar');
 
 toggleBtn.addEventListener('click', () => {
-  if (window.innerWidth <= 768) {
-    sidebar.classList.toggle('open');
-  }
+  sidebar.classList.toggle('open');
 });
